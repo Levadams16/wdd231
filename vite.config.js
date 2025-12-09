8845a15cbac5f85f123d8d991e73ec1f38cb88ae
levadams16 <ada24047@byui.edu>
2025-12-09 10:13:56 -0700

    Nothing changed

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         conditions: resolve(__dirname, "src/conditions.html")
+        
       }
     }
   }
